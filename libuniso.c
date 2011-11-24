7ebe00caeace3b40272b0f506a3e1dfdb13559d9
Natanael Copa <ncopa@alpinelinux.org>
2011-11-24 20:56:40 +0100

    Change license to LGPLv2.1

--- libuniso.c
+++ libuniso.c
@@ -1,15 +1,15 @@
 /* libuniso.c - Unpack ISO9660 File System from a stream
  *
  * Copyright (C) 2011 Timo Teräs <timo.teras@iki.fi>
+ * Copyright (C) 2011 Natanael Copa <ncopa@alpinelinux.org>
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 as published
- * by the Free Software Foundation. See http://www.gnu.org/ for details.
+ * under the terms of the GNU Lesser General Public License version 2.1 as
+ * published by the Free Software Foundation. 
+ *
+ * See http://www.gnu.org/licenses/lgpl-2.1.html for details.
  */
-
-/* Compile with:
- * gcc -std=gnu99 -D_GNU_SOURCE -D_BSD_SOURCE -O2 uniso.c -o uniso */
 
 /*
  * TODO:
--- uniso.c
+++ uniso.c
@@ -1,11 +1,14 @@
 /* uniso.c - Unpack ISO9660 File System from a stream
  *
+ * Copyright (C) 2011 Timo Teräs <timo.traas@iki.fi>
  * Copyright (C) 2011 Natanael Copa <ncopa@alpinelinux.org>
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 as published
- * by the Free Software Foundation. See http://www.gnu.org/ for details.
+ * under the terms of the GNU Lesser General Public License version 2.1 as
+ * published by the Free Software Foundation. 
+ *
+ * See http://www.gnu.org/licenses/lgpl-2.1.html for details.
  */
 
 #include <limits.h>
